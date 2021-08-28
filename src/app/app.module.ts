89c9120dbf0686b47e57cffde24971bf0dcb866f
Ketan <you@example.com>
2021-08-28 13:06:36 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientLoginComponent } from './components/patient-login/patient-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientLoginComponent
   ],
   imports: [
     BrowserModule,
